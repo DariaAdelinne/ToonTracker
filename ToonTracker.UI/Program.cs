b11c55e109eb0e7d4fa70a44c7fd13d651a0807e
David Buraga <davidburaga10@gmail.com>
2026-05-13 20:07:30 +0300

    Adaugat comentarii fisiere UI

--- ToonTracker.UI/Program.cs
+++ ToonTracker.UI/Program.cs
@@ -1,20 +1,44 @@
-// Author: Echipa ToonTracker
-// Functionalitate: Punctul de pornire pentru interfata grafica WinForms.
+/**************************************************************************
+ *                                                                        *
+ *  File:        Program.cs                                               *
+ *  Copyright:   (c) 2026, Echipa ToonTracker                             *
+ *  Description: Punctul de pornire pentru interfata grafica WinForms.    *
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation. This program is distributed in the      *
+ *  hope that it will be useful, but WITHOUT ANY WARRANTY; without even   *
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR   *
+ *  PURPOSE. See the GNU General Public License for more details.         *
+ *                                                                        *
+ **************************************************************************/
 using ToonTracker.Data;
 using ToonTracker.Domain;
 using ToonTracker.Services;
 
 namespace ToonTracker.UI;
 
+/// <summary>
+/// Clasa statica ce serveste drept punct de intrare principal pentru aplicatie.
+/// </summary>
 internal static class Program
 {
+    /// <summary>
+    /// Punctul de intrare principal (Main) pentru aplicatia ToonTracker.
+    /// Configureaza infrastructura de date, serviciile si lanseaza interfata grafica.
+    /// </summary>
     [STAThread]
     private static void Main()
     {
+        // Initializeaza setarile de configurare ale aplicatiei WinForms
         ApplicationConfiguration.Initialize();
+        // Stabileste calea catre fisierul de persistenta JSON in folderul AppData al utilizatorului
         var dataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "ToonTracker", "shows.json");
+        // Initializarea repository-ului generic pentru obiecte de tip AnimatedShow
         var repository = new JsonRepository<AnimatedShow>(dataPath);
+        // Initializarea serviciului care gestioneaza logica de business a aplicatiei
         var service = new ShowService(repository);
+        // Porneste bucla de mesaje a interfetei grafice prin deschiderea ferestrei principale
         Application.Run(new MainForm(service));
     }
 }
--- ToonTracker.UI/ShowForm.cs
+++ ToonTracker.UI/ShowForm.cs
@@ -1,5 +1,18 @@
-// Author: Echipa ToonTracker
-// Functionalitate: Dialog pentru adaugarea si editarea unui desen/serial animat.
+/**************************************************************************
+ *                                                                        *
+ *  File:        ShowForm.cs                                              *
+ *  Copyright:   (c) 2026, Echipa ToonTracker                             *
+ *  Description: Dialog pentru adaugarea si editarea unui desen/serial animat.*
+ *                                                                        *
+ *  This program is free software; you can redistribute it and/or modify  *
+ *  it under the terms of the GNU General Public License as published by  *
+ *  the Free Software Foundation. This program is distributed in the      *
+ *  hope that it will be useful, but WITHOUT ANY WARRANTY; without even   *
+ *  the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR   *
+ *  PURPOSE. See the GNU General Public License for more details.         *
+ *                                                                        *
+ **************************************************************************/
+
 using ToonTracker.Domain;
 using ToonTracker.Services;
 
